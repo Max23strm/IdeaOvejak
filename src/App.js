b8d0ced4733bc7b828a0615803aefcd8af7ded5c
Max23strm <maximiliano.ovejak@gmail.com>
2021-11-24 09:56:06 -0500

    Se creo la barra de navegacion, con archivos de imagen. Se creo la carpeta de componentes y estilos, y se elimino los archivos iniciales de React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar';
 function App() {
+
   return (
     <div className="App">
+        <NavBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-          <h1>Proyecto Ecommerce de CoderHouse</h1>
+        <h1>Proyecto Ecommerce de CoderHouse</h1>
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Usuario from '../imagenes/usuario.png'
+import Carrito from '../imagenes/carrito.png'
+import Menu from '../imagenes/menu.png'
+import Cerrar from '../imagenes/cerrar.png'
+import '../styles/NavBar.css'
+
+
+function NavBar() {
+
+    
+    return (
+        <nav>
+            <h2>LaCasaca</h2>
+            <section className="opciones" id="opciones">
+                <img src={Cerrar} alt="cerrar" className="botonCerrar" />
+                <ul className="opcionesNavegacion">
+                    <li className="opcionNavegacion">INICIO</li>
+                    <li className="opcionNavegacion">DEPORTES 
+                        <ul>
+                            <li>Futbol</li>
+                            <li>Rugby</li>
+                            <li>Basquetball</li>
+                        </ul>
+                    </li>
+                    <li className="opcionNavegacion">CATEGORIAS 
+                        <ul>
+                            <li>Camisetas</li>
+                            <li>Shorts</li>
+                            <li>Conjuntos</li>
+                            <li>Accesorios</li>
+                            <li>Novedades</li>
+                            <li>Ofertas</li>
+                        </ul>
+                    </li>
+                    <li className="opcionNavegacion">CONTACTO</li>
+                </ul>
+            </section>
+                <section className="iconos">
+                    <a href="none" id="menu"><img src={Menu} alt="menu" className="menu"></img></a> 
+                    <a href="none"><img src={Usuario} alt="usuario"></img></a>
+                    <a href="none"><img src={Carrito} alt="carrito"></img></a>
+                </section>
+        </nav>
+    )
+}
+
+export default NavBar
