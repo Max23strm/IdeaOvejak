c7687e855c034afbdd95e6e1e334dec113562a34
Max23strm <maximiliano.ovejak@gmail.com>
2021-12-03 07:18:35 -0500

    Se agregaron los archivos ItemCount.js e ItemCount.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 function App() {
 
   return (
     <div className="App">
         <NavBar/>
+        <ItemCount stock="2" initial="1"/>
         <ItemListContainer/>
+
     </div>
   );
 }
---
+++ src/components/ItemCount.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import '../styles/ItemCount.css'
+
+function ItemCount({stock, initial, onAdd}) {
+    const[cantidad, setCantidad]= useState(parseInt(initial))
+
+    return (
+        <div className="Contador">
+                <h5>Nombre de Item</h5>
+            <section >
+                <button onClick={()=>cantidad<=1?setCantidad(cantidad):setCantidad(cantidad-1)}>&#45;</button>
+
+                <p>{stock<=0?"0":cantidad}</p>
+                
+                <button onClick={()=>cantidad<stock?setCantidad(cantidad+1):setCantidad(cantidad)}>&#43;</button>
+            </section>
+            <button className={stock<=0?"sinStock":"enStock"}  disabled={stock<=0?true:false} onClick={()=>console.log(cantidad)}>{stock<=0?"No disponible":"Agregar al carrito"}</button>
+        </div>
+    )
+}
+export default ItemCount
