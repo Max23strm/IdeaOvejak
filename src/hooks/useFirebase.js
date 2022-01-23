ac87a1b2d149cf74a78538334a8549cbfe51801f
Max23strm <maximiliano.ovejak@gmail.com>
2022-01-23 07:41:41 -0500

    Arreglado el error que no me permitia correr la aplicacion ni usar firebase, retoques de estilo

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,45 +1,11 @@
-import { useState } from 'react/cjs/react.development'
+import { useState } from 'react'
 import{addDoc, collection, getDocs, getDoc, doc} from "firebase/firestore"
-//import db from "../service"
-// Import the functions you need from the SDKs you need
+import db from "../service"
 
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-
-    apiKey: "AIzaSyBt4tHMCq7d0yfhg_oYpXEvHcBoR7kS6RA",
-
-    authDomain: "ecommerce-28185.firebaseapp.com",
-
-    projectId: "ecommerce-28185",
-
-    storageBucket: "ecommerce-28185.appspot.com",
-
-    messagingSenderId: "503979312960",
-
-    appId: "1:503979312960:web:ff082608c48d285894dca5"
-
-};
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-const db= getFirestore(app)
-
-//export default db
 
 function useFirebase() {
-    const[producto, setProducto]=useState({})
-    const[productos, setProductos]=useState([])
+    const [producto, setProducto]=useState({})
+    const [productos, setProductos]=useState([])
     
     //BUSCA UNA COLECCION
     const fetchGetDataCollection=async()=>{
